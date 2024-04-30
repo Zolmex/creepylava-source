a44a6e72dd85d0116ff34a819eddeaadb1bb96b4
Zolmex <zolmexyt@gmail.com>
2024-04-30 16:57:29 -0300

    Source upload

---
+++ server/fame/list.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Xml;
+using common;
+using System.Xml.Serialization;
+using System.IO;
+using MySql.Data.MySqlClient;
+using System.Web;
+using System.Collections.Specialized;
+
+namespace server.fame
+{
+    class list : RequestHandler
+    {
+        public override void HandleRequest(HttpListenerContext context)
+        {
+            NameValueCollection query;
+            using (StreamReader rdr = new StreamReader(context.Request.InputStream))
+                query = HttpUtility.ParseQueryString(rdr.ReadToEnd());
+
+            DbChar character = null;
+            if (query["accountId"] != null)
+            {
+                character = Database.LoadCharacter(
+                    int.Parse(query["accountId"]),
+                    int.Parse(query["charId"])
+                );
+            }
+            FameList list = FameList.FromDb(Database, query["timespan"], character);
+            Write(context, list.ToXml().ToString());
+        }
+    }
+}
